b26e8437a20eed411adc91b78c9415d80b965e25
Emil Segerbäck <emilsegerback@gmail.com>
2016-02-12 20:01:07 +0100

    Add example input file

--- python/main.py
+++ python/main.py
@@ -1,4 +1,15 @@
 import sys
+#from enum import Enum
+
+#class Furniture(Enum):
+#    bed = 1
+#    couce = 2
+#    desk = 3
+#    chair = 4
+#    tv = 5
+#    table = 6
+#    rug = 7
+#    shelf = 8
 
 f = open(sys.argv[1], 'r')
 print(f.read())
