1e017baabcb4479a38059e44bc70744d6ab39a8d
Frans Skarman <frans.skarman@gmail.com>
2016-02-13 07:49:41 +0100

    Add updated furniture data

--- data/furnitureData.js
+++ data/furnitureData.js
@@ -1,1 +1,2 @@
-furnitures = [["door",145,500],["window",150,0],["window",350,0],["bed",55,100],["table",225,240]]
+//furnitures = [["bed", 2,0.5,0], ["bed", -1,-1,90], ["couch", -1,2, 270], ["chair", 0, 0, 0]];
+furnitures = [["door",145,500,0],["window",150,0, 90],["window",350,0, 180],["bed",55,100, 0],["chair",225,240, 0]]
